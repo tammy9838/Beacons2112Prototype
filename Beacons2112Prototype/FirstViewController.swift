423f2b767d2d60215657b3c22a75e6c3edea9094
incloud company <inclouddev27@gmail.com>
2016-06-16 14:20:17 +0700

    Initial Commit

---
+++ Beacons2112Prototype/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Beacons2112Prototype
+//
+//  Created by incloud company on 6/16/2559 BE.
+//  Copyright © 2559 incloud. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
